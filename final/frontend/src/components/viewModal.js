5c642997b1546e4ca4e771e402bce77b591cc954
nico1aseth <96373254+nico1aseth@users.noreply.github.com>
2023-01-06 18:41:56 +0800

    Update viewModal.js

--- final/frontend/src/components/viewModal.js
+++ final/frontend/src/components/viewModal.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-import TextField from '@material-ui/core/TextField';
+import TextField from '@mui/material/TextField';
 import Modal from '@mui/material/Modal';
 import Rating from '../components/rating'
 
